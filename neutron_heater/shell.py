6941b1c731224c284ab19909fc711ec4016fefb7
Slawek Kaplonski <skaplons@redhat.com>
2023-07-24 16:42:32 +0200

    Added concurrency option and possibility to create resources in parallel

--- neutron_heater/shell.py
+++ neutron_heater/shell.py
@@ -10,6 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from concurrent import futures
 import os
 import socket
 import sys
@@ -78,6 +79,15 @@ def register_config_options(conf):
                    default='RegionOne',
                    dest='region_name',
                    help="Cloud's region name."),
+        cfg.IntOpt('concurrency',
+                   default=0,
+                   help='Number of threads in which networks and resources '
+                        'which belongs to network will be created. '
+                        'Each thread will create one network and all subnets '
+                        'and ports which should be created per network. '
+                        'Default value is "0" which means it will be '
+                        'calculated automatically based on the "networks" '
+                        'value.'),
     ]
     conf.register_cli_opts(options)
 
@@ -91,38 +101,61 @@ def get_network_name(index, hostname):
     return "network-%s-host-%s" % (index, hostname)
 
 
-def create_network_with_ports(config):
+def create_network_with_ports(net_number, ipv4_subnets, ipv6_subnets, ports,
+                              hostname, os_client, os_vif):
+    network_name = get_network_name(net_number, hostname)
+    LOG.info("Starting to create network %s" % network_name)
+    network = os_client.create_network(network_name)
+    if not network:
+        LOG.error("Failed to create network. Stopping worker.")
+        return False
+    subnets = {}
+    for v4_subnet in range(ipv4_subnets):
+        subnet_name = "v4_subnet-%s-host-%s" % (v4_subnet, hostname)
+        cidr = V4_CIDR_BASE % v4_subnet
+        subnet = os_client.create_subnet(network['id'], subnet_name, cidr)
+        if subnet:
+            subnets[subnet['id']] = subnet
+
+    for v6_subnet in range(ipv6_subnets):
+        subnet_name = "v6_subnet-%s-host-%s" % (v6_subnet, hostname)
+        cidr = V6_CIDR_BASE % v4_subnet
+        subnet = os_client.create_subnet(network['id'], subnet_name, cidr)
+        if subnet:
+            subnets[subnet['id']] = subnet
+
+    if subnets:
+        # Only if some subnets were created creating ports makes any sense
+        for port in range(ports):
+            port_name = "port-%s-host-%s" % (port, hostname)
+            port = os_client.create_port(network['id'], port_name, hostname)
+            if port:
+                os_vif.plug_port(port, subnets)
+    return True
+
+
+def create_resources(config):
     hostname = socket.gethostname()
     client = openstack_client.OSClient(cloud=config.cloud_name,
                                        region_name=config.region_name)
     os_vif = os_vif_client.OSVifClient()
 
-    for net in range(config.networks):
-        network = client.create_network(get_network_name(net, hostname))
-        if not network:
-            sys.exit(NET_CREATE_FAIL)
-        subnets = {}
-        for v4_subnet in range(config.ipv4_subnets):
-            subnet_name = "v4_subnet-%s-host-%s" % (v4_subnet, hostname)
-            cidr = V4_CIDR_BASE % v4_subnet
-            subnet = client.create_subnet(network['id'], subnet_name, cidr)
-            if subnet:
-                subnets[subnet['id']] = subnet
+    workers = config.concurrency or config.networks
+    with futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        [executor.submit(
+            create_network_with_ports,
+            net, config.ipv4_subnets, config.ipv6_subnets, config.ports,
+            hostname, client, os_vif) for net in range(config.networks)]
 
-        for v6_subnet in range(config.ipv6_subnets):
-            subnet_name = "v6_subnet-%s-host-%s" % (v6_subnet, hostname)
-            cidr = V6_CIDR_BASE % v4_subnet
-            subnet = client.create_subnet(network['id'], subnet_name, cidr)
-            if subnet:
-                subnets[subnet['id']] = subnet
 
-        if subnets:
-            # Only if some subnets were created creating ports makes any sense
-            for port in range(config.ports):
-                port_name = "port-%s-host-%s" % (port, hostname)
-                port = client.create_port(network['id'], port_name, hostname)
-                if port:
-                    os_vif.plug_port(port, subnets)
+def clean_network_with_ports(network, os_client, os_vif):
+    ports = os_client.get_ports(network['id'])
+    for port in ports:
+        subnets = os_client.get_port_subnets(port)
+        os_vif.unplug_port(port, subnets)
+        os_client.delete_port(port)
+    os_client.delete_network(network)
+
 
 def clean_all(config):
     hostname = socket.gethostname()
@@ -132,15 +165,13 @@ def clean_all(config):
     all_networks = client.get_networks()
     expected_network_names = [get_network_name(net, hostname) for
                               net in range(config.networks)]
-    for network in all_networks:
-        if network['name'] not in expected_network_names:
-            continue
-        ports = client.get_ports(network['id'])
-        for port in ports:
-            subnets = client.get_port_subnets(port)
-            os_vif.unplug_port(port, subnets)
-            client.delete_port(port)
-        client.delete_network(network)
+    networks_to_clean = [network for network in all_networks if
+                         network['name'] in expected_network_names]
+    workers = config.concurrency or config.networks
+    with futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        [executor.submit(
+            clean_network_with_ports, network, client, os_vif) for
+            network in networks_to_clean]
 
 
 def main(argv=sys.argv[1:]):
@@ -150,7 +181,7 @@ def main(argv=sys.argv[1:]):
     config(argv)
     if config.action == CREATE:
         LOG.info("Starting resource creation")
-        create_network_with_ports(config)
+        create_resources(config)
         LOG.info("Resources created")
     elif config.action == CLEAN:
         LOG.info("Starting resource cleanup")
